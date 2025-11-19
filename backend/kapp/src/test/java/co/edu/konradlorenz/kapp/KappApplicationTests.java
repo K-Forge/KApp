41eac660bb74e03e8615287ed8b2c1ea5994a1b9
13rian Vargas <bsvargasc13@gmail.com>
2025-11-18 19:43:34 -0500

    [FEAT][CONFIG] Packages Base
    He creado la estructura de paquetes completa para el backend de K-APP, siguiendo la arquitectura en capas y los módulos definidos en el SRS.
    
    Para ver la estructura detallada, lee el README.md

---
+++ backend/kapp/src/test/java/co/edu/konradlorenz/kapp/KappApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.konradlorenz.kapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
